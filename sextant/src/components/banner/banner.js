67fd91f37aeaf7b8c50ad33a4ff2d246186accec
DGarg375 <gargdivyansh11@gmail.com>
2023-01-25 15:20:07 -0800

    banner fix

--- sextant/src/components/banner/banner.js
+++ sextant/src/components/banner/banner.js
@@ -4,7 +4,7 @@ function Banner() {
     return(
         <div class="header-main-container">
             <div class="title-container">
-                Sextant <span id="trademark-symbol"> ® ™ </span>
+                Sextant <span id="trademark-symbol"><div>™</div><div>®</div><div>©</div></span>
             </div>
         </div>
     );
