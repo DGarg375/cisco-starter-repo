ba2522b4ae01ba73ca82ab99ba7e71fa686abf18
DGarg375 <gargdivyansh11@gmail.com>
2023-01-25 16:37:58 -0800

    banner fix issue and create exhibit area

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Banner from './components/banner/banner.js';
+import Exhibit from './components/exhibit/exhibit.js';
 
 function App() {
   return (
     <div className="App">
-        <Banner />
+      <Banner />
+        <div class="exhibit-area">
+          hello there
+        </div>
     </div>
   );
 }
---
+++ sextant/src/components/exhibit/exhibit.js
@@ -0,0 +1,9 @@
+import './exhibit.css';
+
+function Exhibit() {
+    return(
+        <div></div>
+    );
+}
+
+export default Exhibit;
